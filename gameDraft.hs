1d5cc29bbb7dc35a1edea981574b2deb77836d4b
dhirendesai <45038689+dhirendesai@users.noreply.github.com>
2019-04-23 17:46:21 +0100

    Add files via upload

--- gameDraft.hs
+++ gameDraft.hs
@@ -1,12 +1,4 @@
-{--
-This is the basic interface of the game:
-need to add more options about the different rooms
-need to add more options inside the function description
 
-Further development could be utilized the type declaration of the new data structure to store
-the default rooms' attributes, and enable the user to have the interactions with the rooms' objects,
-for example, pick up the swords, etc.
---}
 import System.IO 
 import Data.Char
 
@@ -15,9 +7,9 @@ import Data.Char
 strlen' :: IO ()
 strlen'= do putStr "Enter the username: "
             xs <- getLine
-            putStr "the username is: "
+            putStr "The username is: " 
             putStr xs
-            putStrLn " "
+            putStrLn ",  Enter Game to play"
 game :: IO ()
 game = do putStrLn "Input which direction "
           direction <- getLine
@@ -39,21 +31,27 @@ match xs ys = description xs ys
 --need to add more options of the rooms, for example
 -- description "s" "room4"= "you are inside room4"
 description :: String->String -> String
-description "w" "room1" =
+description "n" "room1" =
     "You are in a room1.  To the north is the dark mouth\n" ++
     "of a room2; to the south is a small room5.  Your\n" ++
     "assignment, should you decide to accept it, is to\n" ++
     "recover the famed Bar-Abzad ruby and return it to\n" ++
     "this room3."
+description "s" "room1" =
+    "You are in the mouth of a dank room2.  The exit is to\n" ++
+    "the south; there is a large, dark, round passage to\n" ++
+    "the east."
 description "s" "room2" = " You are inside room2 at south direction. Congratulations!!  You have recovered the ruby and won the game."
 description "w" "room2" = " You are inside room2 at west direction. Congratulations!!  You have recovered the key to entre next level."
 description "n" "room2" = " You are inside room2 at north direction. Congratulations!!  Your life is increased "
 description "e" "room2" = " You are inside room2 at east direction. Congratulations!!  You have killed all your enemy, you can go to next level."
 
-description "s" "room1" =
-    "You are in the mouth of a dank room2.  The exit is to\n" ++
-    "the south; there is a large, dark, round passage to\n" ++
-    "the east."
+description "s" "room3" = " You are inside room2 at south direction. Congratulations!!  You have recovered the torch to see in dark."
+description "w" "room3" = " You are inside room2 at west direction. Congratulations!!  You have recovered extra life to go long."
+description "n" "room3" = " You are inside room2 at north direction. Congratulations!!  You have recovered binocular. "
+description "e" "room3" = " You are inside room2 at east direction. Congratulations!!  You have recovered lessor gun."
+
+
 ---description "s" "room4"= "you are inside room4"
 description "s" "room4" = " You are inside room4 at south direction. Congratulations!!  You have recovered Sword to kill enemy."
 description "w" "room4" = " You are inside room4 at west direction. Congratulations!!  You have recovered Pistol to shoot enemy."
