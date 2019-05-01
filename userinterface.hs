a2df92ae5b7edc5ab3fa446c0dea2aa3e5440a10
dhirendesai <45038689+dhirendesai@users.noreply.github.com>
2019-05-01 09:59:52 +0100

    Add files via upload

---
+++ userinterface.hs
@@ -0,0 +1,34 @@
+import System.IO
+
+strlen' :: IO ()
+strlen'= do putStr "Enter the username: "
+            xs <- getLine
+            putStr "the username is : "
+            putStr xs
+            putStrLn " "
+                     
+game    ::  IO ()
+game =  do  putStrLn "Welcome to the text adventure game!"
+            putStrLn instructions
+            play "start game" 
+--            return "Goodbye!"
+
+instructions =
+    "Enter commands using one or two words.\n" ++
+    "Available commands are:\n" ++
+    "game               -- to start the game.\n" ++
+    "n  s  e  w  u  d   -- to go in that direction.\n" ++
+    "take object        -- to pick up the named object.\n" ++
+    "drop object        -- to put down the named object.\n" ++
+    "kill               -- to attack an enemy.\n" ++
+    "look               -- to look around you again.\n" ++
+    "i                  -- to see your inventory (what you are holding).\n" ++
+    "quit               -- to end the game and quit."
+play :: String -> IO ()
+play direction =
+   do guess <- getLine
+      if guess == "quit" then
+         putStrLn "You won/game over!"
+      else
+         do putStrLn "check condition"
+            play direction	 
---
+++ weapons.hs
@@ -0,0 +1,21 @@
+
+
+data Weapon = Weapon {weaponDamage:: Int,weaponHitChance :: Float, weaponName :: String } deriving (Show,Eq)
+
+bfg = Weapon 100 0.9 "BFG"
+highlander = Weapon 100 0.9 "Highlander"
+loic = Weapon 100 1 "Low Orbital Ion Cannon"
+
+swordOfAwesome = Weapon 50 0.8 "Sword of Awesome"
+rocketLauncher = Weapon 70 0.5 "Rocket Launcher"
+revolver = Weapon 40 0.9 "Revolver"
+
+blockSword = Weapon 30 0.9 "Block Sword"
+pistol = Weapon 30 0.9 "Pistol"
+bow=Weapon 40 0.5 "Bow"
+
+fists = Weapon 20 1 "Fists"
+
+weakWeapons=[blockSword,pistol,bow]
+mediocreWeapons=[swordOfAwesome,rocketLauncher,revolver]
+strongWeapons=[bfg,highlander,loic]
